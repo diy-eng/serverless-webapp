81ec5dd514c02f628849cafc3f0e98cb71f2e43f
Patino <92400538+diy-eng@users.noreply.github.com>
2022-04-23 15:14:04 -0500

    Add cors to Lambda

--- tf/main.tf
+++ tf/main.tf
@@ -2,6 +2,44 @@ provider "aws" {
   region  = "us-east-2"
 }
 
+#########
+# S3 SITE
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = "www.${var.bucket_name}"
+  acl = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags = var.common_tags
+}
+
+# S3 bucket for redirecting non-www to www.
+resource "aws_s3_bucket" "root_bucket" {
+  bucket = var.bucket_name
+  acl = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+
+  website {
+    redirect_all_requests_to = "https://www.${var.domain_name}"
+  }
+
+  tags = var.common_tags
+}
+
+########
+# LAMBDA
+
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
 
@@ -20,6 +58,15 @@ module "lambda_function" {
 resource "aws_lambda_function_url" "test_latest" {
   function_name      = module.lambda_function.lambda_function_arn
   authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
 }
 
 output "lambda_url" {
